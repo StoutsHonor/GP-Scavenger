9c8fa625e5055a155dd89ffd1dfc732c35201041
startupjen <jentran39@gmail.com>
2017-08-29 18:42:39 -0700

    Confirmed getCurrentLocation button works and submit

---
+++ client/src/components/MapStoreLocationButton.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Button
+} from 'react-native';
+
+
+class MapStoreLocationButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+
+    const styles = StyleSheet.create({
+      button: {
+        height: 50,
+        width: 50,
+        backgroundColor: 'white',
+        borderRadius: 10,
+        borderColor: '#000000'
+
+      },
+      buttonContainer: {
+        zIndex: 9999999,
+        left: 165,
+        bottom: 20
+      }
+    })
+
+    return(
+      <View style={styles.buttonContainer} >
+        <TouchableOpacity onPress={this.props.storeMarker}>
+          <Text>Submit</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+}
+
+export default MapStoreLocationButton;
