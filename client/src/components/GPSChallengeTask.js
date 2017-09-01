0df5763fe77d1e7f896ece37027ed8f23211d4bc
startupjen <jentran39@gmail.com>
2017-08-31 18:07:46 -0700

    Map playthrough works

--- client/src/components/GPSChallengeTask.js
+++ client/src/components/GPSChallengeTask.js
@@ -20,17 +20,19 @@ class GPSChallengeTask extends Component {
       description: '',
       time_limit: '',
       markers: [],
-      currentLocation: []
+      currentLocation: [],
+      message: ''
     }
   }
 
   componentWillMount() {
-    console.log(`this is ${JSON.stringify(this.props.currentChallenge)}`)
-    if (this.props.currentChallenge.location) {
-      //console.log(`nextProps in GPSChallengeTask is ${JSON.stringify(this.props.currentChallenge)}`)
-      
+    console.log(`this is componentWillMount in GPSChallengeTask ${JSON.stringify(this.props.currentChallenge)}`)
       this.setState({ markers: [{ latitude: this.props.currentChallenge.location[0], longitude: this.props.currentChallenge.location[1] }] })
-    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log(`this is componentWillReceiveProps in GPSChallengeTask ${JSON.stringify(this.props.currentChallenge)}`)
+      this.setState({ markers: [{ latitude: nextProps.currentChallenge.location[0], longitude: nextProps.currentChallenge.location[1] }] })
   }
 
   onMapMarkerSubmit(location) {
@@ -44,15 +46,18 @@ class GPSChallengeTask extends Component {
     this.setState({currentLocation: location})
     console.log(`this.state.markers is ${JSON.stringify(this.state.markers)}`)
     console.log(`this.state.markers[0].latitude is typeof ${typeof this.state.markers[0].latitude} and location.latitude typeof is ${typeof location.latitude}}`)
-    if (this.state.markers[0].latitude.toFixed(3) === location.latitude.toFixed(3) && this.state.markers[0].longitude.toFixed(3) === location.longitude.toFixed(3)) {
+    if (this.state.markers[0].latitude.toFixed(2) === location.latitude.toFixed(2) && this.state.markers[0].longitude.toFixed(2) === location.longitude.toFixed(2)) {
       console.log('YEAH RIGHT LOCATION')
+      this.props.challengeCompleted()
     } else {
       console.log('WRONG LOCATION TRY AGAIN!!!')
+      this.setState({message: 'NOT THERE YET KEEP MOVIN!'})
     }
   }
 
   render() {
     console.log(`GPSChallengeTask this.state.markers: ${JSON.stringify(JSON.stringify(this.state.markers))}`)
+    console.log(`this.props.currentChallenge.location is ${this.props.currentChallenge.location}`)
     return(
       <View style={{padding:10}}>
         <Map onMarkerSubmit={this.onMapMarkerSubmit} markers={this.state.markers } />
@@ -60,6 +65,7 @@ class GPSChallengeTask extends Component {
         <Text>Description: {this.props.currentChallenge.description}</Text>
         <Text>Description: {this.props.currentChallenge.location}</Text>
         <Text>Current Location is: {JSON.stringify(this.state.currentLocation)}</Text>
+        <Text>{this.state.message}</Text>
       </View>
     )
   }
