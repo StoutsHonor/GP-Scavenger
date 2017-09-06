ecf9e2b5176e52bba832a1ce41e4ea1085213512
miken619 <miken619@gmail.com>
2017-09-06 16:24:45 -0700

    Working on lobby

--- server/index.js
+++ server/index.js
@@ -5,9 +5,6 @@ const http = require('http');
 const socketio = require('socket.io');
 
 const db = require('./database');
-// const db = require('./database/db.js');
-// const https = require('https');
-// const fs = require('fs');
 
 const app = express();
 
@@ -18,16 +15,8 @@ app.use(parser.urlencoded({extended: true}))
     console.log('Server: received POST to /*');
     res.status(201).send('server response to POST to /*');
   });
-// app.use(express.static(__dirname + '/client-react'));
 
-// const sslOptions = {
-//   key: fs.readFileSync('./server/key.pem'),
-//   cert: fs.readFileSync('./server/cert.pem'),
-//   requestCert: false,
-//   rejectUnauthorized: false
-// };
-
-let port = false || 3000;
+let port = 3000;
 
 const server = http.Server(app);
 const websocket = socketio(server);
@@ -35,70 +24,20 @@ server.listen(port, function () {
   console.log(`Server: listening on ${port}`);
 });
 
-// https.createServer(sslOptions, app).listen(port, function () {
-//   console.log(`Server: listening on ${port}`);
-// });
-
-
-
-// Mapping objects to easily map sockets and users.
-var clients = {};
-var users = {};
-
-// This represents a unique chatroom.
-// For this example purpose, there is only one chatroom;
-var chatId = 1;
-let send_messages = [];
 
 websocket.on('connection', (socket) => {
 
-    socket.on('createRoom', roomName => {
-      socket.join(roomName);
-    });
-  
-    //socket.on('userJoined', (userId) => onUserJoined(userId, socket));
-    socket.on('message', (message) => {
-      //onMessageReceived(message, socket)
-      socket.in(message.roomName).emit('message', message);
-      //socket.in('room2').emit('message', message);
-    });
-});
-
-// Event listeners.
-// When a user joins the chatroom.
-function onUserJoined(userId, socket) {
+  socket.on('createRoom', (message) => {
+    socket.join(message.roomName);
+    socket.in(message.roomName).emit('joinLobby', message.userId);
+  });
  
-  users[socket.id] = userId;
-  _sendExistingMessages(socket);
-    
-  
-  
-}
-
-// When a user sends a message in the chatroom.
-function onMessageReceived(message, senderSocket) {
-  console.log('Socket received message ', message);
-
-  senderSocket.emit('message', [message]);
-}
-
-
-var stdin = process.openStdin();
-stdin.addListener('data', function(d) {
-  let val = { 
-    _id: Math.round(Math.random() * 1000000),
-    text: d.toString().trim(),
-    user: {
-            _id: 2,
-            name: 'React Native'
-          },
-    createdAt: new Date(),    
-  };
-  send_messages.push(val);
-  websocket.emit('message', val);
-  //onMessageReceived({ message: d.toString().trim() }, socket);
-  
+  socket.on('message', (message) => { 
+    socket.in(message.roomName).emit('message', message);
+  });
 });
+
+
 
 
 
