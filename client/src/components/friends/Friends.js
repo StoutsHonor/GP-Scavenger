3a3cb1139a5b0d0391ba1c4b383a0551ed975da4
miken619 <miken619@gmail.com>
2017-09-12 10:08:47 -0700

    Added functionality to friends component

--- client/src/components/friends/Friends.js
+++ client/src/components/friends/Friends.js
@@ -10,47 +10,100 @@ import {
 import { Actions } from 'react-native-router-flux';
 import SideMenu from 'react-native-side-menu';
 import HomePage from '../HomePage';
+import Swiper from 'react-native-swiper';
+import config from "../../../config/config";
+import { connect } from "react-redux";
+import { GiftedChat } from "react-native-gifted-chat";
 
-const Friends = () => {
-  return (
-    <SideMenu menu={<HomePage/>}>
-      <View style={styles.container}>
-        <View style={styles.profile}>
 
-          <View style={styles.name}>
-            <View style={styles.image}>
+
+const mapStateToProps = state => {
+  return {
+    userId: state.client.userIdentity,
+    gameId: state.play.gameId,
+    gameInfo: state.play.gameInfo,
+    challenges: state.play.allChallenges,
+    gamePoints: state.play.gamePoints,
+    currentGameTeam1: state.play.currentGameTeam1,
+    currentGameTeam2: state.play.currentGameTeam2
+  };
+};
+
+class Friends extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onSend = this.onSend.bind(this);
+
+    this.state = {
+      messages: [],
+      user: {
+        _id: 1,
+        name: this.props.userId
+      }
+    }
+  }
+
+  onSend() {
+
+  }
+
+  render() {
+    return (
+      <SideMenu menu={<HomePage/>}>
+        <View style={styles.container}>
+          <View style={styles.profile}>
+
+            <View style={styles.name}>
+              <View style={styles.image}>
+              </View>
+              <View>
+                <Text style={styles.description}>John Doe</Text>
+                <Text style={styles.digits}>Software Engineer Trainee at Hack Reactor</Text>
+              </View>
             </View>
-            <View>
-              <Text style={styles.description}>John Doe</Text>
-              <Text style={styles.digits}>Software Engineer Trainee at Hack Reactor</Text>
+            <View style={styles.info}>
+              <View style={styles.stats}>
+                <Text style={styles.digits}>45</Text>
+                <Text style={styles.description}>Game</Text>
+                <Text style={styles.description}>Played</Text>
+              </View>
+              <View style={styles.stats}>
+                <Text style={styles.digits}>25</Text>
+                <Text style={styles.description}>Game</Text>
+                <Text style={styles.description}>Won</Text>
+              </View>
+              <View style={styles.stats}>
+                <Text style={styles.digits}>33</Text>
+                <Text style={styles.description}>Friends</Text>
+              </View>
+            </View>
+            <View style={styles.friend}>
+              <Button title={'Friends'}/>
             </View>
           </View>
-          <View style={styles.info}>
-            <View style={styles.stats}>
-              <Text style={styles.digits}>45</Text>
-              <Text style={styles.description}>Game</Text>
-              <Text style={styles.description}>Played</Text>
+          <View style={styles.body}>
+          <Swiper style={styles.wrapper} >
+            <View style={styles.slide1}>
+              
             </View>
-            <View style={styles.stats}>
-              <Text style={styles.digits}>25</Text>
-              <Text style={styles.description}>Game</Text>
-              <Text style={styles.description}>Won</Text>
+            <View style={styles.slide2}>
+              
             </View>
-            <View style={styles.stats}>
-              <Text style={styles.digits}>33</Text>
-              <Text style={styles.description}>Friends</Text>
+            <View style={styles.slide3}>
+              <GiftedChat
+                messages={this.state.messages}
+                onSend={this.onSend}
+                user={this.state.user}
+              />
             </View>
+          </Swiper>
           </View>
-          <View style={styles.friend}>
-            <Button title={'Friends'}/>
-          </View>
+
         </View>
-        <View style={styles.body}>
-        </View>
-
-      </View>
-    </SideMenu>
-  );
+      </SideMenu>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -114,9 +167,39 @@ const styles = StyleSheet.create({
   },
   description: {
     fontSize: 16
-  }
-
-
+  },
+  wrapper: {
+  },
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB'
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5'
+  },
+  slide3: {
+    flex: 1,
+    width: Dimensions.get("window").width - 15,
+    height: Dimensions.get("window").height / 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9'
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  slide: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'transparent'
+  },
 });
 
-export default Friends;
+export default connect(mapStateToProps)(Friends);
